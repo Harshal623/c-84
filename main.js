0a4f0051eae81aff36289ce565f0c8e05dbde644
Harshal623 <89270049+Harshal623@users.noreply.github.com>
2021-11-13 09:23:16 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+canvas = document.getElementById("My_canvas")
+ctx = canvas.getContext("2d")
+rover_x = 100
+rover_y = 150
+rover_width = 100
+rover_height = 100
+background_image = "mars.jpg"
+rover_image = "rover.png"
+function add(){
+    bg_img = new Image()
+    bg_img.onload = uploadbg
+    bg_img.src = background_image
+    rover_img = new Image()
+    rover_img.onload = uploadrover
+    rover_img.src = rover_image
+}
+function uploadbg(){
+    ctx.drawImage(bg_img, 0, 0, canvas.width, canvas.height)
+}
+function uploadrover(){
+    ctx.drawImage(rover_img, rover_x, rover_y, rover_width, rover_height)
+}
+window.addEventListener("keydown", My_keydown)
+function My_keydown(e){
+    keypressed = e.keyCode
+    console.log(keypressed)
+    if (keypressed == "37"){
+        console.log("left key is pressed")
+        Left()
+    }
+    if (keypressed == "38"){
+        console.log("up key is pressed")
+        Up()
+    }
+    if (keypressed == "40"){
+        console.log("down key is pressed")
+        Down()
+    }
+    if (keypressed == "39"){
+        console.log("right key is pressed")
+        Right()
+    }
+}
